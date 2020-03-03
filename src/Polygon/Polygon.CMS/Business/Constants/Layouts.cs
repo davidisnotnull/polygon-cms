ea6e1d6ceb58a082ac844382b484459da8edda9b
David Johnson <davidisnotnull@outlook.com>
2020-03-03 11:04:46 +0000

    Building out the SCSS

---
+++ src/Polygon/Polygon.CMS/Business/Constants/Layouts.cs
@@ -0,0 +1,25 @@
+﻿namespace Polygon.CMS.Business.Constants
+{
+    public static class Layouts
+    { 
+        /// <summary>
+        /// Main layout for all Polygon pages (excluding forms and modals)
+        /// </summary>
+        public const string MainLayout = "~/Pages/Polygon/Shared/_MainLayout.cshtml";
+
+        /// <summary>
+        /// Layout for partial views
+        /// </summary>
+        public const string PartialLayout = "~/Pages/Polygon/Shared/_PartialLayout.cshtml";
+
+        /// <summary>
+        /// Layout for all content within the Global Modal
+        /// </summary>
+        public const string ModalLayout = "~/Pages/Polygon/Shared/_ModalLayout.cshtml";
+
+        /// <summary>
+        /// Layout for the 
+        /// </summary>
+        public const string ModalFormLayout = "~/Pages/Polygon/Shared/_ModalFormLayout.cshtml";
+    }
+}
