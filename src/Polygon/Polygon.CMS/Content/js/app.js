6c3290c5fc6a8671249c15e7a4ec9fed0a9df80f
David Johnson <davidisnotnull@outlook.com>
2020-03-11 01:19:57 +0000

    Add webpack as front end build framework

---
+++ src/Polygon/Polygon.CMS/Content/js/app.js
@@ -0,0 +1,8 @@
+﻿import $ from "jquery";
+import _ from "lodash";
+import "popper.js";
+import "bootstrap";
+
+$(function() {
+    console.log("Testing jquery import");
+})
---
+++ src/Polygon/Polygon.CMS/webpack.config.js
@@ -0,0 +1,103 @@
+﻿const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./Content/js/app.js"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false
+                    }
+                }
+            })
+        ],
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./wwwroot/dist")
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: "./wwwroot/dist/images"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: "./wwwroot/dist/fonts"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ]
+    }
+}
