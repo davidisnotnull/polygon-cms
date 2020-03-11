9253ebd5902f6158d63b5d1b8580011696e70106
David Johnson <davidisnotnull@outlook.com>
2020-03-11 10:39:50 +0000

    Implement SCSS compilation and bundling in webpack

--- src/Polygon/Polygon.CMS/Content/js/app.js
+++ src/Polygon/Polygon.CMS/Content/js/app.js
@@ -2,6 +2,9 @@
 import _ from "lodash";
 import "popper.js";
 import "bootstrap";
+import "./components/global-modal.js";
+
+import "../scss/app.scss";
 
 $(function() {
     console.log("Testing jquery import");
