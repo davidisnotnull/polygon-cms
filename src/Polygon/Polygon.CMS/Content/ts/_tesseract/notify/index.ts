bbacce63d7b52a1de8d0aa9e6bf4c83c0685c2a7
David Johnson <davidisnotnull@outlook.com>
2020-04-20 12:41:25 +0100

    Implement Tesseract Notify

--- src/Polygon/Polygon.CMS/Content/ts/_tesseract/notify/index.ts
+++ src/Polygon/Polygon.CMS/Content/ts/_tesseract/notify/index.ts
@@ -1,33 +1,76 @@
 import NotifyOptions from "./interfaces/NotifyOptions";
+const utils = require('../_utils');
+import './styles';
 
 class TesseractNotify {
     
     props: NotifyOptions;
     
     constructor(props: NotifyOptions) {
+        this.props = props;
         this.init();
     }
     
     init() {
-        switch (this.props.type)
-        {
-            case "success": 
-                break;
-            case "info":
-                break;
-            case "error": 
-                break;
-            default:
-                break;
-        }
+        this.showNotification();
     }
     
     showNotification() {
+        let body = document.querySelector("body");
+        let notification = <HTMLDivElement>(document.createElement('div'));
+        notification.className = "tesseract__notify";
+
+        switch (this.props.type)
+        {
+            case "success":
+                notification.classList.add("success");
+                break;
+            case "info":
+                notification.classList.add("info");
+                break;
+            case "error":
+                notification.classList.add("error");
+                notification.innerHTML = "<strong>ERROR</strong> ";
+                break;
+            default:
+                notification.classList.add("default")
+                break;
+        }
+
+        notification.innerHTML += this.props.message;
+        notification.addEventListener('click', this.hideNotification.bind(this));
+        body.append(notification);
         
+        let showAnimation = notification.animate([
+            { opacity: 0 },
+            { opacity: 1}
+        ], { duration: 500, iterations: 1});
+        showAnimation.onfinish = this.notificationTimer;
+    }
+    
+    notificationTimer = function() {
+        let timerDuration: number = 5000;
+        let notification = document.querySelector(".tesseract__notify");
+        setTimeout(() => {
+            let hideAnimation = notification.animate([
+                { opacity: 1 },
+                { opacity: 0 }
+            ], {duration: 500, iterations: 1});
+            hideAnimation.onfinish = function() {
+                notification.remove();
+            };
+        }, timerDuration)
     }
     
     hideNotification() {
-        
+        let notification = document.querySelector(".tesseract__notify");
+        let hideAnimation = notification.animate([
+            { opacity: 1 },
+            { opacity: 0 }
+        ], {duration: 500, iterations: 1});
+        hideAnimation.onfinish = function() {
+            notification.remove();
+        };
     }
     
 }
--- src/Polygon/Polygon.CMS/Content/ts/pages/developer/tesseract/index.ts
+++ src/Polygon/Polygon.CMS/Content/ts/pages/developer/tesseract/index.ts
@@ -1,12 +1,26 @@
+import TesseractNotify from '../../../_tesseract/notify/';
+import NotifyOptions from "../../../_tesseract/notify/interfaces/NotifyOptions";
 
 class TesseractDeveloperPage {
 
     constructor() {
-        
+        this.init();
     }
     
     init() {
-        
+        let notifyTrigger = document.querySelector("#FireNotification");
+        notifyTrigger.addEventListener('click', this.fireNotification.bind(this));
+    }
+    
+    fireNotification() {
+        console.info("Firing Tesseract notification");
+        let notifyOptions = new class implements NotifyOptions {
+            message: string;
+            type: string;
+        }
+        notifyOptions.message = "Here's your test notification";
+        notifyOptions.type = "error";
+        new TesseractNotify(notifyOptions);
     }
     
 }
